4bd33be62527b094dcc786c7af3c976f2b3c25d7
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-13 15:41:50 -0300

    Adicionados itens a lista e melhorias visuais a View Store

--- AppShopping/AppShopping/App.xaml.cs
+++ AppShopping/AppShopping/App.xaml.cs
@@ -10,6 +10,8 @@ namespace AppShopping
         {
             InitializeComponent();
 
+            //Device.SetFlags(new string[] {""})
+
             MainPage = new Menu();
         }
 
--- AppShopping/AppShopping/Services/EstablishmentService.cs
+++ AppShopping/AppShopping/Services/EstablishmentService.cs
@@ -16,11 +16,34 @@ namespace AppShopping.Services
                 {
                     Id = 1,
                     Type = EstablishmentType.Store,
-                    Logo = "",
+                    Logo = "https://logodownload.org/wp-content/uploads/2014/07/renner-logo-2-1.png",
                     Name = "Renner",
                     Description = "A Lojas Renner S.A. lorem ipsum",
                     Adress = "3 andar - Loja 10 - Setor Norte",
                     Phone = "(61) 3333-3333", 
+                    },
+
+
+                new Establishment()
+                {
+                    Id = 2,
+                    Type = EstablishmentType.Store,
+                    Logo = "https://www.anacouto.com.br/wp-content/uploads/2021/07/GALERIA-SITE-AMERICANAS.png",
+                    Name = "Americanas",
+                    Description = "A Lojas Americanas S.A. lorem ipsum",
+                    Adress = "1 andar - Loja 4 - Setor Norte",
+                    Phone = "(61) 2222-2222",
+                    },
+
+                new Establishment()
+                {
+                    Id = 3,
+                    Type = EstablishmentType.Store,
+                    Logo = "https://gkpb.com.br/wp-content/uploads/2021/01/novo-logo-burger-king-2021.png",
+                    Name = "Burger King",
+                    Description = "O Burger King, lorem ipsum",
+                    Adress = "2 andar - Loja 9 - Setor Norte",
+                    Phone = "(61) 3333-3333",
                     }
             };
             return establishments;
