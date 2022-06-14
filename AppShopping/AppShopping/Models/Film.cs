b1d36407f1cf7dcc3ff3eb2842a76b6ea9ffb1ed
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-14 15:02:56 -0300

    Adicionados View e ViewModel para FilmDetail e listagem de sessões

--- AppShopping/AppShopping/Models/Film.cs
+++ AppShopping/AppShopping/Models/Film.cs
@@ -9,5 +9,6 @@ namespace AppShopping.Models
         public string Cover { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<SessionGroup> SessionGroups { get; set; }
     }
 }
---
+++ AppShopping/AppShopping/Models/SessionGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping.Models
+{
+    public class SessionGroup : List<String>
+    {
+        public string Name { get; set; }
+        public SessionGroup()
+        {
+
+        }
+        public SessionGroup(string name, List<String> list) : base(list)
+        {
+            Name = name;
+            
+        }
+    }
+}
