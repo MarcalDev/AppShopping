eb8ad41150df04e614e89cfa2106c571f757c8c0
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-14 15:41:36 -0300

    Iniciada a seralização de atributos em FilmDetail

--- AppShopping/AppShopping/ViewModels/FilmDetailViewModel.cs
+++ AppShopping/AppShopping/ViewModels/FilmDetailViewModel.cs
@@ -1,19 +1,31 @@
 ﻿using AppShopping.Models;
 using AppShopping.Services;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xamarin.Forms;
 
 namespace AppShopping.ViewModels
 {
+    [QueryProperty("filmSerialized", "filmSerialized")]
     public class FilmDetailViewModel
     {
         public Film Film { get; set; }
 
+        public string filmSerialized {
+            set {
+                // Deserializar e atribuir ao Film
+                var decode = Uri.UnescapeDataString(value);
+                var film = JsonConvert.DeserializeObject<Film>(decode);
+
+                Film = film;
+            }
+        }
         public FilmDetailViewModel()
         {
-            Film = new CinemaService().GetFilms().First();
+
         }
     }
 }
