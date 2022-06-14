2cf4c1ff2baa4d3c02a00df9829548c2f033f094
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-14 13:59:42 -0300

    Adicionados View e ViewModel e Service para Cinema, Model Film,

---
+++ AppShopping/AppShopping/Models/Film.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping.Models
+{
+    public class Film
+    {
+        public string Cover { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
