27c3a1da9a2190449a42b1556dbb74397968479e
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-14 10:49:13 -0300

    Adicionados conversor e binding para o tipo de estabelecimento

---
+++ AppShopping/AppShopping/LIbraries/Converters/EstablishmentTypeConversor.cs
@@ -0,0 +1,24 @@
+﻿using AppShopping.LIbraries.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppShopping.LIbraries.Converters
+{
+    public class EstablishmentTypeConversor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            EstablishmentType type = (EstablishmentType)value;
+
+            return (type == EstablishmentType.Store) ? "Loja" : "Restaurante";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
