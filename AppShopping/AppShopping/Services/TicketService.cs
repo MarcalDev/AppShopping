7cc2254a62d72309eb54481aab0bc6f4a5633a69
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-15 09:18:25 -0300

    Adicionados Model e Service para a Ticket View e enumerador para o status do Ticket

---
+++ AppShopping/AppShopping/Services/TicketService.cs
@@ -0,0 +1,38 @@
+﻿using AppShopping.LIbraries.Enums;
+using AppShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppShopping.Services
+{
+    public class TicketService
+    {
+        private List<Ticket> fakeTickets = new List<Ticket>()
+        {
+            new Ticket() { Number = "1555551515161", StartDate = new DateTime(2020, 10, 20, 16, 02, 32), Status = TicketStatus.paid },
+            new Ticket() { Number = "4646449994646", StartDate = new DateTime(2020, 10, 20, 18, 56, 42), Status = TicketStatus.paid },
+            new Ticket() { Number = "3535353534646", StartDate = new DateTime(2020, 10, 20, 20, 32, 01), Status = TicketStatus.paid },
+            
+        };
+
+        public Ticket GetTicketInfo(string number)
+        {
+            var endDate = new DateTime(2020, 10, 20, 22, 00, 00);
+
+            var ticket = fakeTickets.FirstOrDefault(a => a.Number == number);
+
+            if (ticket == null)
+                throw new Exception("Ticket não encontrado!");
+
+            if (ticket.Status == TicketStatus.paid)
+                throw new Exception("Ticket já pago!");
+
+            ticket.EndDate = endDate;
+            ticket.Price = 6.00m;
+
+            return ticket;
+        }
+    }
+}
---
+++ AppShopping/AppShopping/ViewModels/TicketScanViewModel.cs
@@ -0,0 +1,42 @@
+﻿using AppShopping.LIbraries.Helpers.MVVM;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace AppShopping.ViewModels
+{
+    public class TicketScanViewModel : BaseViewModel
+    {
+        public string TicketNumber { get; set; }
+        public ICommand TicketScanCommand { get; set; }
+        private string _message;
+        public string Message {
+            get
+            {
+                return _message;
+            }
+            set 
+            {
+                SetProperty(ref _message, value);
+            } 
+        }
+
+        public ICommand TicketPaidHistoryCommand { get; set; }
+        public TicketScanViewModel()
+        {
+            TicketScanCommand = new Command(TicketScan);
+            TicketPaidHistoryCommand = new Command(TicketPaidHistory);
+        } 
+        
+        private void TicketScan()
+        {
+
+        }
+        private void TicketPaidHistory()
+        {
+
+        }
+    }
+}
