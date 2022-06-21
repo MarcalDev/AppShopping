ba10f1332fadcbb936241959e5a32c2c5e82a922
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-21 10:38:39 -0300

    Adicionados View, ViewModel, e model para News (Tela de novidades)

---
+++ AppShopping/AppShopping/Services/NewsService.cs
@@ -0,0 +1,22 @@
+﻿using AppShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping.Services
+{
+    public class NewsService
+    {
+        private List<News> fakeNews = new List<News>()
+        {
+            new News() { Title = "DIA DAS MÃES", Description="A cada R$ 100,00 em compras você ganha um cupom para concorre a 5 lindos Toyota Fit para sua mãe."},
+            new News() { Title = "NATAL", Description="O natal está chegando"},
+            
+        };
+
+        public List<News> GetNews()
+        {
+            return fakeNews;
+        }
+    }
+}
