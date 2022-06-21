5435edd3e1c43551ecddde47fe5e105a44c3e0aa
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-21 11:25:33 -0300

    Adicionadas classe e interface para WifiConnector

---
+++ AppShopping/AppShopping.Android/Libraries/Helpers/Connect/WifiConnector.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Net.Wifi;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppShopping.Droid.Libraries.Helpers.Connect;
+using AppShopping.LIbraries.Helpers.Connect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WifiConnector))]
+namespace AppShopping.Droid.Libraries.Helpers.Connect
+{
+    public class WifiConnector : IWifiConnector
+    {
+        [Obsolete]
+        public void ConnectToWifi(string ssid, string password)
+        {
+            
+            var wifiManager = (WifiManager)Android.App.Application.Context
+                        .GetSystemService(Context.WifiService);
+
+            var formattedSsid = $"\"{ssid}\""; // var formattedSsid = "NOMEDAREFEWIFI"
+            var formattedPassword = $"\"{password}\"";
+
+            var wifiConfig = new WifiConfiguration
+            {
+                Ssid = formattedSsid,//OI_ELIAS_4G
+                PreSharedKey = formattedPassword
+            };
+
+            var addNetwork = wifiManager.AddNetwork(wifiConfig);
+
+            var network = wifiManager.ConfiguredNetworks
+                 .FirstOrDefault(n => n.Ssid == $"\"{ssid}\""); // OI_ELIAS_4G
+
+            if (network == null)
+            {
+                wifiManager.Disconnect();
+                var enableNetwork = wifiManager.EnableNetwork(network.NetworkId, true);
+                return;
+            }
+        }
+    }
+}
---
+++ AppShopping/AppShopping.iOS/Libraries/Helpers/Connect/WifiConnector.cs
@@ -0,0 +1,21 @@
+﻿using AppShopping.iOS.Libraries.Helpers.Connect;
+using AppShopping.LIbraries.Helpers.Connect;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WifiConnector))]
+namespace AppShopping.iOS.Libraries.Helpers.Connect
+{
+    public class WifiConnector : IWifiConnector
+    {
+        public void ConnectToWifi(string ssid, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
