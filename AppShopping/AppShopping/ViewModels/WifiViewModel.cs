d2870df30410a04e6d3abf8bc6281398beb2f2f2
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-06-21 10:52:53 -0300

    Adicionados ViewModel e View para Wifi (Tela de conexão ao Wifi)

---
+++ AppShopping/AppShopping/ViewModels/WifiViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AppShopping.LIbraries.Helpers.MVVM;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace AppShopping.ViewModels
+{
+    public class WifiViewModel : BaseViewModel
+    {
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public ICommand ConnectToWifiCommand { get; set; }
+
+        public WifiViewModel()
+        {
+            ConnectToWifiCommand = new Command(ConnectToWifi);
+        }
+
+        private void ConnectToWifi()
+        {
+
+        }
+    }
+}
